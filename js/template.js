bee6af02d5ada4203d39da47e0a3320507f79bed
0yqc <0yqc@duck.com>
2024-11-30 17:58:26 +0100

    2024/11/30

--- js/template.js
+++ js/template.js
@@ -1,4 +1,3 @@
-// Navigator:
 const path = window.location.pathname.split("/").filter(part => part); //Split into Each Link Part, seperated by "/", then filter out all empty parts
 var link = ""; //set link starting point to / (https://0yQc.github.io/) to add further elements
 var text_navigator = "<a href=" + link + "/>root</a>"; // set text for first element, since it is not fetched in window.location.pathname
@@ -6,7 +5,7 @@ for (i = 0; i < path.length; i++) { //Iterate through every path element
     link = link + "/" + path[i]; //set the current link, with the link before, and the new path element
     text_navigator = text_navigator + " > <a href=" + link + ">" + path[i] + "</a>";
     /*
-    *   text = text +: Add to the text
+    *   text_navigator = text_navigator +: Add to the text
     *   " >: Add > as an arrow symbol
     *   <a href=": Start Link
     *   + link: use link as the href
@@ -15,10 +14,25 @@ for (i = 0; i < path.length; i++) { //Iterate through every path element
     *   + "</a>": Close the <a> tag
     */
 }
-//document.getElementById("header").innerHTML = text;
 
-// Last Modified:
-const text_last_modified = "Last Modified: " + new Date(document.lastModified).toLocaleDateString("en-US", { month: "long", day: "numeric", year: "numeric" });
+document.getElementById("header").innerHTML =
+    "<header>" +
+    "<table>" +
+    "<tr>" +
+    "<td>" +
+    "<a href=\"\">" +
+    "<img src=\"media/loggo.svg\" alt=\"yQc: yQlormatic\" style=\"width:87px;height:38,5px\">" +
+    "</a>" +
+    text_navigator +
+    "</td>" +
+    "<td style=\"text-align: right;\">" +
+    "Last Modified: " + new Date(document.lastModified).toLocaleString("en-US", { month: "short", day: "numeric", year: "numeric" }) +
+    "</td>" +
+    "</tr>" +
+    "</table>" +
+    "</header>"
 
-// Header:
-document.getElementById("header").innerHTML = "<table><tr><td>" + text_navigator + "</td><td style=\"text-align: right;\">" + text_last_modified + "</td></tr></table>"
+document.getElementById("footer").innerHTML =
+    "<footer>" +
+    "<a href=\"credits.html\">Credits</a>" +
+    "</footer>"
