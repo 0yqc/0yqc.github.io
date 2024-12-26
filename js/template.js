cfd646a100233a4f5c3fd45f9c2c91676098dd2e
yQc <0yqc@duck.com>
2024-12-26 12:35:43 +0100

    test new template #5

--- js/template.js
+++ js/template.js
@@ -1,8 +1,19 @@
-console.log("template.js loaded")
+console.log("template.js loaded");
+
 fetch("../templates/header.html")
-    .then(response => response.text())
+    .then(response => {
+        console.log("Response status:", response.status);
+        
+        if (!response.ok) {
+            throw new Error(`HTTP error! Status: ${response.status}`);
+        }
+
+        return response.text();
+    })
     .then(data => {
-        console.log(data)
+        console.log("Fetched data:", data);
         document.getElementById("header").innerHTML = data;
     })
-    .catch(error => console.error('Error fetching header:', error));
+    .catch(error => {
+        console.error('Error fetching header:', error);
+    });
