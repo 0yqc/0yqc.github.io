639c278cb1e65b3774edd63a52093d31cb3af709
yQc <0yqc@duck.com>
2024-12-26 12:25:07 +0100

    test new template #2

--- js/template.js
+++ js/template.js
@@ -1,6 +1,6 @@
 fetch("../templates/header.html")
     .then(response => response.text())
     .then(data => {
-        document.getElementById("header").innerHTML = data
-    }
-)
+        document.getElementById("header").innerHTML = data;
+    })
+    .catch(error => console.error('Error fetching header:', error));
