3a810427dce631c4d265a4f893fbe8b9598ab297
0yqc <0yqc@duck.com>
2025-02-02 14:57:08 +0100

    Added Contact Form

---
+++ js/contact_form.js
@@ -0,0 +1,12 @@
+document.getElementById("contact_form").addEventListener("submit", function (event) {
+    event.preventDefault();
+    const name = document.getElementById("name").value
+    const email = document.getElementById("email").value
+    const type = document.querySelector("input[name=\"type\"]:checked").value
+    const unformatedMessage = document.getElementById("message").value
+    var message = unformatedMessage.replace(/\n/g, '%0D%0A');
+
+    console.log(name + email + type + message)
+
+    window.location.href = "mailto:0yqc@duck.com?subject=Website " + type + " by " + name + " (" + email + ")&body=" + message
+})
