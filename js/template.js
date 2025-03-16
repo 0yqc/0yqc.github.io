b84dc26d8ea01a157ae9e8f9231b54ac28328523
0yqc <0yqc@duck.com>
2025-03-16 08:41:15 +0100

    Improved 404 Page, started multi-level dropdown, changed template.js to fetch other files.

--- js/template.js
+++ js/template.js
@@ -1,6 +1,6 @@
+text_navigator = ""
 const path = window.location.pathname.split("/").filter(part => part); //Split into Each Link Part, seperated by "/", then filter out all empty parts
-var link = ""; //set link starting point to / (https://0yQc.github.io/) to add further elements
-var text_navigator = "<a href=" + link + "/>root</a>"; // set text for first element, since it is not fetched in window.location.pathname
+var link = ""; //set link starting point to / to add further elements
 for (i = 0; i < path.length; i++) { //Iterate through every path element
     link = link + "/" + path[i]; //set the current link, with the link before, and the new path element
     text_navigator = text_navigator + " > <a href=" + link + ">" + path[i] + "</a>";
@@ -15,46 +15,19 @@ for (i = 0; i < path.length; i++) { //Iterate through every path element
     */
 }
 
-document.getElementById("header").innerHTML = `
-<header>
-    <table>
-        <tr>
-            <td>
-                <a href="">
-                    <img src="media/logo/white.svg" alt="yQc: yQlormatic" style="width:87px;height:38,5px"></a>
-`
-    + text_navigator +
-    `
-            </td>
-            <td style="text-align: right;">
-                Last Modified: ` + new Date(document.lastModified).toLocaleDateString("en-us", {year: "numeric", month: "short", day: "numeric"}) +
-    `</td>
-        </tr>
-    </table>
-</header>
-`
+function loadFile(path, elementId, callback) {
+    fetch(path)
+        .then(response => response.text())
+        .then(data => {
+            document.getElementById(elementId).innerHTML = data;
+            if (callback) callback();
+        })
+        .catch(error => console.error("Error Loading File: " + path + ", Error: " + error));
+}
 
-document.getElementById("footer").innerHTML = `
-<footer>
-    <table>
-        <tr>
-            <td style="vertical-align: center">
-                <a href="credits">Credits</a><br><br>
-                <a href="contact">Contact<br>Feedback<br>Bug Report</a>
-            </td>
-            <td style="width: 50%; font-size: 75%;">
-                <form id="newsletter">
-                    <label>E-Mail Newsletter Signup</label><br>
-                    <label for="email">Your E-Mail Adress:</label><br>
-                    <input type="email" name="email" id="email" required><br>
-                    <input type="checkbox" name="agree" id="agree" required>
-                    <label for="agree">I agree to receiving update emails about this website, I can opt out from these emails at any time.</label><br>
-                    <input type="submit" name="submit" id="submit" value="Signup (Opens Mail Program)">
-                </form>
-            </td>
-        </tr>
-    </table>
-
-<script src="js/newsletter.js"></script>
-</footer>
-`
+loadFile("templates/header.html", "header", function() {
+    document.getElementById("navigator").innerHTML = text_navigator;
+    document.getElementById("last_modified").innerHTML = new Date(document.lastModified).toLocaleDateString("en-us", {year: "numeric", month:
+        "short", day: "numeric"});
+});
+loadFile("templates/footer.html", "footer");
