86c50528c9c31d74a97616a6f798aa1eb6539abe
0yqc <0yqc@duck.com>
2025-04-10 09:27:41 +0200

    Added RNG, Improved Tracker

--- js/tracker.js
+++ js/tracker.js
@@ -2,10 +2,19 @@ document.getElementById("browser").innerHTML = navigator.product
 document.getElementById("browser_version").innerHTML = navigator.appVersion
 document.getElementById("browser_lang").innerHTML = navigator.language
 document.getElementById("os").innerHTML = navigator.platform
-document.getElementById("referrer").innerHTML = document.referrer
+document.getElementById("cpu").innerHTML = navigator.hardwareConcurrency
+if (document.referrer) {
+	// if it exists
+	document.getElementById("referrer").innerHTML = document.referrer
+} else {
+	// of not, it was accessed directly
+	document.getElementById("referrer").innerHTML = "direct"
+}
 document.getElementById("timezone").innerHTML = Intl.DateTimeFormat().resolvedOptions().timeZone
+document.getElementById("connection").innerHTML = navigator.connection.effectiveType
 
-navigator.getBattery().then(function(battery) {
+
+navigator.getBattery().then(function (battery) {
 	document.getElementById("bat_percentage").innerHTML = (Math.round(battery.level * 100)) + "%"
 	document.getElementById("bat_charging").innerHTML = battery.charging
 	document.getElementById("bat_timeEmpty").innerHTML = (Math.round(battery.dischargingTime / 60)) + " minutes"
